bf800792abc9e4212428265d881d589efc6ccc2a
euri10 <benoit.barthelet@gmail.com>
2015-03-20 16:39:57 +0100

    scraper cpu

---
+++ scrapy_db_builder/scrapy_db_builder/middlewares.py
@@ -0,0 +1,30 @@
+__author__ = 'lotso'
+
+
+# Importing base64 library because we'll need it ONLY in case if the proxy we are going to use requires authentication
+import base64
+
+from ConfigParser import SafeConfigParser, NoSectionError
+
+# Start your middleware class
+class ProxyMiddleware(object):
+    # overwrite process request
+    def process_request(self, request, spider):
+
+        parser = SafeConfigParser()
+        parser.read('config.ini')
+        try:
+            proxyurl = parser.get('proxy_settings', 'proxyurl')
+            username = parser.get('proxy_settings', 'username')
+            password = parser.get('proxy_settings', 'password')
+            # Set the location of the proxy
+
+            request.meta['proxy'] = proxyurl
+
+            # Use the following lines if your proxy requires authentication
+            proxy_user_pass = username+":"+password
+            # setup basic authentication for the proxy
+            encoded_user_pass = base64.encodestring(proxy_user_pass)
+            request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+        except NoSectionError, e:
+            pass
---
+++ scrapy_db_builder/scrapy_db_builder/settings.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_db_builder project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+from ConfigParser import SafeConfigParser, NoSectionError
+
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+try:
+    imagepath = parser.get('image', 'imagepath')
+except NoSectionError, e:
+    print("Check your config file")
+
+
+BOT_NAME = 'scrapy_db_builder'
+
+SPIDER_MODULES = ['scrapy_db_builder.spiders']
+NEWSPIDER_MODULE = 'scrapy_db_builder.spiders'
+DOWNLOADER_MIDDLEWARES = {'scrapy_db_builder.middlewares.ProxyMiddleware': 1}
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_db_builder (+http://www.yourdomain.com)'
+IMAGES_STORE = imagepath
+# 90 days of delay for image expiration
+IMAGES_EXPIRES = 90
+
